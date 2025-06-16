9996d50eb3bf24e6b958c14819c97e6c33ab36f7
Ismael Toala <ismael.toala@epn.edu.ec>
2025-06-16 17:31:33 -0500

    runner

--- calculadora/src/main.ts
+++ calculadora/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Console } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => console.warn('Servidor Levantado'))
+  .catch(() => console.error('Hubo errores'));
